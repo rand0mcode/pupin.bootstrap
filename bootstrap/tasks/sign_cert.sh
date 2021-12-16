d35f1a3e44932775fa28091fc0c9e508a00c573f
Robert Waffen <rwaffen@gmail.com>
2021-12-16 17:46:26 +0100

    check if host is already signed befor attemptng to sign it

--- bootstrap/tasks/sign_cert.sh
+++ bootstrap/tasks/sign_cert.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-puppetserver ca sign --certname $PT_certname
+if puppetserver ca list --certname $PT_certname --format json | jq '.signed[].name' -r | grep $PT_certname; then
+  echo "already signed"
+else
+  puppetserver ca sign --certname $PT_certname
+fi
